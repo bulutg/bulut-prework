b96bbad18cfea9805b82fd49f60d18da2e3e1558
Bulut Gözübüyük <bulutgozubuyuk@gmail.com>
2024-12-29 18:48:10 -0500

    init

--- bulut-prework/ViewController.swift
+++ bulut-prework/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
 
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    @IBAction func changeOtherColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.layer.borderColor = randomColor.cgColor
+        view.layer.borderWidth = 15.0
+    }
 }
 
